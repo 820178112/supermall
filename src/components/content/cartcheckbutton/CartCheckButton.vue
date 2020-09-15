4c507728c858d4b84aa0abb74c750e1f7638532e
820178112 <qq820178112@163.com>
2020-09-15 20:23:34 +0800

    'supermall'

---
+++ src/components/content/cartcheckbutton/CartCheckButton.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "CartCheckButton"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/cart/childComps/CartBottomBar.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "CartButtomBar"
+}
+</script>
+
+<style scoped>
+
+</style>
